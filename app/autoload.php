0c3f9a3ee849d253b227ff3b6df422ebcb9c21e6
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-03-14 22:37:14 +0100

    Delete var_dump

--- app/autoload.php
+++ app/autoload.php
@@ -1,11 +1,8 @@
 <?php
 spl_autoload_register(function ($classname){
-    var_dump($classname);
     $file = __DIR__ . '/' . str_replace('app\\', '', $classname) . '.php';
-    var_dump($file);
     if (!file_exists($file)) {
         $file = '../src/' . str_replace('\\', '/', $classname) . '.php';
-        var_dump($file);
     }
     include_once $file;
 });
