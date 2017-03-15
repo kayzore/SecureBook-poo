409e5c79f280af70eefd01cf5ddafdfb458376f5
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-03-15 23:49:23 +0100

    Test de recuperation de la session d'un utilisateur

--- src/CoreBundle/Controller/CoreController.php
+++ src/CoreBundle/Controller/CoreController.php
@@ -3,11 +3,14 @@
 namespace CoreBundle\Controller;
 
 use ControllerBundle\Model\Controller;
+use UserBundle\Model\Users;
 
 class CoreController extends Controller
 {
     public function accueilAction()
     {
+        $users = new Users();
+        var_dump($users->getUser());
         $this->render('CoreBundle::Home:accueil.html.twig');
     }
 }
