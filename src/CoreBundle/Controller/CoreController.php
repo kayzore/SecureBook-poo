6b917b4fe8a6933f83000edb5855ace5ae1a9b6e
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-03-15 19:22:56 +0100

    Clear CoreController in CoreBundle

--- src/CoreBundle/Controller/CoreController.php
+++ src/CoreBundle/Controller/CoreController.php
@@ -2,12 +2,11 @@
 
 namespace CoreBundle\Controller;
 
-use src\ControllerBundle\Model\Controller;
+use ControllerBundle\Model\Controller;
 
 class CoreController extends Controller
 {
     public function accueilAction()
     {
-        $this->render('CoreBundle:index.html.twig');
     }
 }
