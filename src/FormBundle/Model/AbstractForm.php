20a90ae00c285f24268d3df33fc174d1802f4250
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-03-16 12:31:12 +0100

    Creation du form builder

---
+++ src/FormBundle/Model/AbstractForm.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace FormBundle\Model;
+
+
+abstract class AbstractForm
+{
+    /**
+     * @var $entity
+     */
+    private $entity;
+
+    private $form = array();
+
+    public function __construct()
+    {
+        $this->configureOptions($this);
+    }
+
+    public function createView()
+    {
+        return $this->getForm();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+
+    /**
+     * @param mixed $entity
+     */
+    public function setEntity($entity)
+    {
+        $this->entity = $entity;
+    }
+
+    /**
+     * @return array
+     */
+    public function getForm()
+    {
+        return $this->form;
+    }
+
+    /**
+     * @param array $form
+     */
+    public function setForm(array $form)
+    {
+        $this->form = $form;
+    }
+
+    abstract function configureOptions(AbstractForm $abstractForm);
+}
---
+++ src/FormBundle/Model/FormBuilder.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stagiaire
+ * Date: 16/03/2017
+ * Time: 11:19
+ */
+
+namespace FormBundle\Model;
+
+
+class FormBuilder
+{
+    /**
+     * @var FormBuilder
+     */
+    private $builder;
+    /**
+     * @var $entityName
+     */
+    private $entityName;
+    private $form = array();
+
+    public function add($entity_field, $type_field, array $options)
+    {
+        $attr = $this->checkAttr($entity_field, $options);
+        $this->setForm($entity_field, '<input type="' . $type_field .'" name="_' . $entity_field . '"' . $attr .'>');
+
+        return $this;
+    }
+
+    private function checkAttr($entity_field, $options)
+    {
+        $attr = '';
+        foreach ($options['attr'] as $key => $option) {
+            if ($key == 'class') {
+                $attr .= ' class="' . $option . '"';
+            } elseif ($key == 'id') {
+                $attr .= ' id="' . $this->getEntityName() . '_' . $option . '"';
+            } elseif ($key == 'multiple') {
+                $attr .= ' multiple="' . $option . '"';
+            } elseif ($key == 'placeholder') {
+                $attr .= ' placeholder="' . $option . '"';
+            }
+        }
+        return $attr;
+    }
+
+    /**
+     * @return FormBuilder
+     */
+    public function getBuilder()
+    {
+        return $this->builder;
+    }
+
+    /**
+     * @param FormBuilder $builder
+     * @return FormBuilder
+     */
+    public function setBuilder(FormBuilder $builder)
+    {
+        $this->builder = $builder;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getForm()
+    {
+        return $this->form;
+    }
+
+    /**
+     * @param $field_name
+     * @param string $field
+     * @return FormBuilder
+     */
+    public function setForm($field_name, $field)
+    {
+        $this->form[$field_name] = $field;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEntityName()
+    {
+        return $this->entityName;
+    }
+
+    /**
+     * @param string $entityName
+     * @return FormBuilder
+     */
+    public function setEntityName($entityName)
+    {
+        $this->entityName = $entityName;
+
+        return $this;
+    }
+}
